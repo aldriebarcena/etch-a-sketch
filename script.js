54bc5d9c951b2f33a125a67f4a856a287001e617
Aldrie Barcena <aldrievbarcena@gmail.com>
2022-11-24 13:14:49 -0800

    made a grid like effect with gaps between divs

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-const gridContainer = document.querySelector('#grid-container')
-gridContainer.style.gridTemplateColumns = "repeat(16, 1fr)"
-gridContainer.style.gridTemplateRows = "repeat(16, 1fr)"
+function populateGrid(size) {
+    // select container and create column and rows
+    const gridContainer = document.querySelector('#grid-container')
+    gridContainer.style.gridTemplateColumns = `repeat(${size} , 1fr)`
+    gridContainer.style.gridTemplateRows = `repeat(${size} , 1fr)`
 
+    // for loop to repeat grid, then append to container
 for (let i = 0; i<256; i++) {
     let square = document.createElement('div')
     square.style.backgroundColor = 'grey';
     gridContainer.insertAdjacentElement('beforeend', square)
 }
+}
+populateGrid(16)

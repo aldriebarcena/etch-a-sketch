19f2fef8c9c2354601b68e6f3a9f5bc5eec896a4
Aldrie Barcena <aldrievbarcena@gmail.com>
2022-11-24 12:59:57 -0800

    created grid in divs

---
+++ script.js
@@ -0,0 +1,9 @@
+const gridContainer = document.querySelector('#grid-container')
+gridContainer.style.gridTemplateColumns = "repeat(16, 1fr)"
+gridContainer.style.gridTemplateRows = "repeat(16, 1fr)"
+
+for (let i = 0; i<256; i++) {
+    let square = document.createElement('div')
+    square.style.backgroundColor = 'grey';
+    gridContainer.insertAdjacentElement('beforeend', square)
+}
